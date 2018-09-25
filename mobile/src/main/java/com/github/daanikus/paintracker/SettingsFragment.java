f77f626b2b24e36bc0041edd258345872be06da3
Dan <daanikus@gmail.com>
2018-09-25 15:19:44 +1200

    Began adding settings menu with notifications option

---
+++ mobile/src/main/java/com/github/daanikus/paintracker/SettingsFragment.java
@@ -0,0 +1,43 @@
+package com.github.daanikus.paintracker;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+
+    }
+
+   @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_notif")) {
+            //Preference pref = findPreference(key);
+            //pref.setSummary(sharedPreferences.getString(key, ""));
+            MainActivity.notificationsOn = sharedPreferences.getBoolean("pref_notif", true);
+            Toast.makeText(getView().getContext(), "Listener noted change", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
