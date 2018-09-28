86fd3cab37b0e7259c447d0d7dd97654b8d1480b
emcfadzean <emilio.k.m@gmail.com>
2018-09-28 13:45:55 +1200

    Added toolbar to StatsActivity, for back navigation.

--- mobile/src/main/java/com/github/daanikus/paintracker/StatsActivity.java
+++ mobile/src/main/java/com/github/daanikus/paintracker/StatsActivity.java
@@ -2,6 +2,7 @@ package com.github.daanikus.paintracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        Toolbar toolbar = findViewById(R.id.toolbar_stats);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
