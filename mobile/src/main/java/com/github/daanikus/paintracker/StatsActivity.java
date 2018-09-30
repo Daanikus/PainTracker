2df80c181350598bd6fede34daf8ecb62cd93cb4
Dan <daanikus@gmail.com>
2018-10-01 11:49:17 +1300

    Added some getters to stats

---
+++ mobile/src/main/java/com/github/daanikus/paintracker/StatsActivity.java
@@ -0,0 +1,38 @@
+package com.github.daanikus.paintracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+        Toolbar toolbar = findViewById(R.id.toolbar_stats);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Date date = new Date(Stats.getMostRecent());
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, ''yy h:mm a");
+
+        TextView mostRecent = (TextView) findViewById(R.id.mostRecent);
+        mostRecent.setText("Most Recent: "+ sdf.format(date));
+
+        TextView maxPain = (TextView) findViewById(R.id.maxPain);
+        maxPain.setText("Maximum: "+Stats.getMax());
+
+        TextView minPain = (TextView) findViewById(R.id.minPain);
+        minPain.setText("Minimum: "+Stats.getMin());
+
+        TextView avePain = (TextView) findViewById(R.id.avePain);
+        avePain.setText("Average: "+Stats.getAvePainLevel());
+
+
+    }
+}
